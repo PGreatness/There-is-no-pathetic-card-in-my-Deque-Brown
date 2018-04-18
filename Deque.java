ca2d26ccc9693b602f9b1b7c059148d2f565bef5
Peter <pcwalina@stuy.edu>
2018-04-18 14:00:41 -0400

    added deque interface

---
+++ Deque.java
@@ -0,0 +1,12 @@
+public interface Deque{
+   
+    public void addFirst(String e);
+    public void addLast(Strig e);
+ /*-8-8-8-8-8Move ME-8-8-8-8-8-8    
+    public String peekFirst();
+    public String peekLast();
+    public String removeLast();
+    public String removeFirst();
+    public int Size();
+    8-8-8-8-8-88-8-8-8--8-8-8-8-8-*/
+}
