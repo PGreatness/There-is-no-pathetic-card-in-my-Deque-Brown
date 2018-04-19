3bbc06f3017fa388e24b17ea9a89dd8e46b19966
Ahnaf Hasan <ahasan1@stuy.edu>
2018-04-18 21:27:56 -0400

    modified Deque to have removeFirstOccurrence and removeLastOccurrence

--- Deque.java
+++ Deque.java
@@ -1,12 +1,28 @@
-public interface Deque{
-   
-    public void addFirst(String e);
-    public void addLast(Strig e);
- /*-8-8-8-8-8Move ME-8-8-8-8-8-8    
-    public String peekFirst();
-    public String peekLast();
-    public String removeLast();
-    public String removeFirst();
-    public int Size();
-    8-8-8-8-8-88-8-8-8--8-8-8-8-8-*/
+/******************************************************************
+ * Interface Deque<T>
+ * A double-ended queue where elems can enter or exit from either
+ * side. Queue has infinite capacity and is represented via doubly
+ * linked nodes. 
+ * Stores type T where T is a typed-variable
+ *****************************************************************/
+
+public interface Deque<T> {
+    //add to the beginning of the queue
+    public void addFirst(T x);
+    //add to the end of queue
+    public void addLast(T x);
+    //return the first elem w/o altering queue
+    public T peekFirst();
+    //return last elem w/o altering queue
+    public T peekLast();
+    //# of meaningful items in queue
+    public int size();
+    //return the first elem and remove it
+    public T removeFirst();
+    //return the last elem and remove it
+    public T removeLast();
+    //true if the first occurrence of the val is removed, false else
+    public boolean removeFirstOccurrence(T x);
+    //true if the last occurrence of the val is removed, false else
+    public boolean removeLastOccurrence(T x);
 }
